abcc4198dfd7d9f03fd343f9778f5d461caf6a13
unknown <louisblebohec@gmail.com>
2017-07-29 13:14:17 -0600

    final commit

--- assets/script.js
+++ assets/script.js
@@ -14,9 +14,14 @@ function renderButtons() {
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
         // Adds a class of movie to our button
-        a.addClass("searcher");
+        a.attr("class", "searcher");
+        a.attr("z-index", 500000);
+        // a.attr("onclick", "function(){ helpIveBeenClicked(); }");
         // Added a data-attribute
-        a.attr("data-name", giphysearch[i]);
+        console.log(a.attr("class"));
+        var dataname = giphysearch[i].split(" ").join("+");
+        a.attr("data-name", dataname);
+        console.log("The data-name attr is " + a.attr("data-name"));
         // Provided the initial button text
         a.text(giphysearch[i]);
         // Added the button to the buttons-view div
@@ -25,6 +30,27 @@ function renderButtons() {
 }
 
 
+function queryItUp(movie){
+    var search = movie;
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        search + "&api_key=b0c19a35d4eb44dcb0f9193929975e60&limit=25";
+    // Creates AJAX call for the specific movie button being clicked
+    console.log("Help!! I've been clicked!!");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+        $("#gifs-view").empty();
+        for(var i = 0; i<response.data.length; i++){
+            var a = $("<div>");
+            console.log(response.data[i].embed_url);
+            a.html("<img src='" + response.data[i].images.fixed_height.url + "'>");
+            $("#gifs-view").append(a)
+        }
+    })
+}
 
 // Calling the renderButtons function to display the intial buttons
 $(document).ready(function () {
@@ -42,4 +68,17 @@ $(document).ready(function () {
     });
 
 });
+
+
+// function helpIveBeenClicked(){
+//     console.log("Help, I've been clicked!!");
+// }
+//
+$("body").on("click", ".searcher", function () {
+    console.log("Help! I've been clicked!!");
+    var data = $(this).attr("data-name");
+    console.log("This is not the data you are looking for: " + data);
+    queryItUp(data);
+});
+
 console.log("JS is online");
